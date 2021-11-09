f5d2c56998f4597a3c0283c37570a1eae8d68c24
RONiN <Roninowns@gmail.com>
2021-11-09 20:40:29 +0530

    Registered AttributesProvider & OperatorProvider

--- src/main/java/me/illusion/datasync/DataSyncPlugin.java
+++ src/main/java/me/illusion/datasync/DataSyncPlugin.java
@@ -59,6 +59,7 @@ public class DataSyncPlugin extends JavaPlugin {
     }
 
     private void registerProviders() {
+        storageHandler.registerProvider(new AttributesProvider());
         storageHandler.registerProvider(new FoodProvider());
         storageHandler.registerProvider(new GamemodeProvider());
         storageHandler.registerProvider(new HealthProvider());
@@ -67,7 +68,9 @@ public class DataSyncPlugin extends JavaPlugin {
         storageHandler.registerProvider(new PotionProvider());
         storageHandler.registerProvider(new SaturationProvider());
         storageHandler.registerProvider(new EnderchestProvider());
-        storageHandler.registerProvider((new ExperienceProvider()));
+        storageHandler.registerProvider(new ExperienceProvider());
+        storageHandler.registerProvider(new OperatorProvider());
+
     }
 
     private void registerPackets() {
